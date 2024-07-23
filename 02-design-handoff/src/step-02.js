96364a52ef2107d5f0919a43f20208daf5ad0caf
ehh99 <jinung5@gmail.com>
2024-07-23 16:55:17 +0900

    리액트전 자바스크립트 수업 완료

--- 02-design-handoff/src/step-02.js
+++ 02-design-handoff/src/step-02.js
@@ -3,14 +3,14 @@
 // [x] STEP 2. Class Programming
 // [ ] STEP 3. Web Components API
 // ---------------------------------------------------------
-import SortableList from "./SortableList.js";
+import SortableList from './SortableList.js';
 
 // Class => Instance
 // API 1. new Class()
-const sortableList1 = new SortableList(".list");
+const sortableList1 = new SortableList('.list');
 console.log(sortableList1);
 
 // 클래스 재사용(reuse)
 // API 2. Class.create()
-const sortableList2 = SortableList.create(".another-list");
+const sortableList2 = SortableList.create('.another-list');
 console.log(sortableList2);
